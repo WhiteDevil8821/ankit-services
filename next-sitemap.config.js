176e73ba3050f4a82d89cb58d4356ab9b85315aa
WhiteDevil8821 <ankit.kumar221099@gmail.com>
2025-05-01 15:27:31 +0530

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ankit-services.in', // Replace with your actual domain
+    generateRobotsTxt: true, // (optional)
+};
